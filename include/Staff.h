9aa90a0138e711b4933bd1a27aede06d5692a570
Nick Khomiakov <72936218+nixonthefourth@users.noreply.github.com>
2026-03-23 17:43:59 +0000

    feat: added the proper loaning functionality to speed up the workflow

--- include/Staff.h
+++ include/Staff.h
@@ -12,8 +12,28 @@
  * @brief Inherits from parental class Person. Sets staff member's resource borrow limit as 2.
  */
 class Staff : public Person {
+private:
+    int resourcesBorrowed;
+
 public:
-    Staff(int ID, std::string name) : Person(2, ID, name) {}
+    Staff(int ID,
+        std::string name,
+        int resourcesBorrowed) : Person(2, ID, name), resourcesBorrowed(resourcesBorrowed) {}
+
+    // Public accessors
+    /**
+     *
+     * @return Returns the current number of borrowed items
+     */
+    int getResourcesBorrowed() const {
+        return resourcesBorrowed;
+    }
+
+    /**
+     *
+     * @param newResourcesBorrowed Sets new resource borrow number
+     */
+    void setResourcesBorrowed(int newResourcesBorrowed) { resourcesBorrowed = newResourcesBorrowed; }
 };
 
 #endif //LIBRARY_SYS_STAFF_H
--- include/Student.h
+++ include/Student.h
@@ -11,9 +11,29 @@
  * @brief Inherits from parental class Person. Sets student's resource borrow limit as 1.
  */
 class Student : public Person {
+private:
+    int resourcesBorrowed;
 public:
-    Student(int ID, const std::string &name)
-        : Person(1, ID, name) {} // Students can borrow up to 1 resource
+    Student(int ID,
+        const std::string &name,
+        int resourcesBorrowed)
+        : Person(1, ID, name),
+        resourcesBorrowed(resourcesBorrowed) {} // Students can borrow up to 1 resource
+
+    // Public Accessors
+    /**
+     *
+     * @return Returns the current number of borrowed items
+     */
+    int getResourcesBorrowed() const {
+        return resourcesBorrowed;
+    }
+
+    /**
+     *
+     * @param newResourcesBorrowed Sets new resource borrow number
+     */
+    void setResourcesBorrowed(int newResourcesBorrowed) { resourcesBorrowed = newResourcesBorrowed; }
 };
 
 #endif //LIBRARY_SYS_STUDENT_H
